3fcf7a636ca240ae22de2f485f2c47125a9bac7d
Marina Delatorre <marina_mzd@yahoo.com.br>
2024-05-06 12:26:09 +0200

    Support extraction of multiple instances per item

--- src/data/extract_details.py
+++ src/data/extract_details.py
@@ -13,17 +13,18 @@ def extract_data(results: list[dict]) -> dict:
 
     """
     extracted_data = {}
+    item_data = {}
     for result in results:
-        item_data = {}
+        item_label = result['item']['value'].split('/')[-1]
+        extracted_data[item_label] = item_data
         for label, value in result.items():
             label = label.replace('Label', '') if label.endswith('Label') or \
                 label in ('start', 'end', 'duration', 'publicationdate') else label+'_wikidata'
-            if isinstance(value, dict) and 'value' in value:
-                item_data[label] = value['value'].split('T')[0] if label in ('start', 'end', 'publicationdate') \
-                    else value['value'].split('/')[-1]
+            item_data[label] = {} if label not in item_data.keys() else item_data[label]
+            if isinstance(value, dict) and label in ('start', 'end', 'publicationdate'):
+                item_data[label][value['value'].split('T')[0]] = {}
             else:
-                item_data[label] = value.get('value', None).split('/')[-1]
-        extracted_data[result['item']['value'].split('/')[-1]] = item_data
+                item_data[label][value.get('value', None).split('/')[-1]] = {}
     return extracted_data
 
 
@@ -66,12 +67,13 @@ def main():
     for data_type, data_dict in collection_dict.items():
         for _, value in result_dict.items():
             for _, v in value.items():
-                data_dict[v.get(f'{data_type}_wikidata', data_type)] = v.get(data_type, None)
-
-        
+                if any("wikidata" in word for word in v.keys()) and data_type in v.keys():
+                    data_dict.update(dict(zip(v.get(f"{data_type}_wikidata").keys(), v.get(data_type, {}).keys())))
+                # data_dict[next(iter(v.get(f'{data_type}_wikidata', data_type)))] = v.get(data_type, None)
+    
     for data_type, data_dict in collection_dict.items():
-        data_dict.pop(data_type, None)
-        write_to_json(data_dict, f"{data_type}_details.json")
+        data_dict.pop(None, None)
+        write_to_json(data_dict, f"data/processed/{data_type}_details.json")
 
     write_to_json(result_dict, "data/processed/detailed_items.json")
 
--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -1,4 +1,30 @@
-from utils.utils import read_from_json
+from utils.utils import read_from_json, write_to_json
+
+
+def format_items(data: dict) -> dict:
+    """
+    Formats the items dictionary by removing empty dictionaries and converting
+    the inner dictionaries to lists.
+
+    Args:
+        data (dict): A dictionary containing item data.
+
+    Returns:
+        dict: A formatted dictionary containing item data.
+    """
+    new_data = {}
+    for outer_key, outer_value in data.items():
+        new_data[outer_key] = {}
+        for key, value in outer_value.items():
+            new_data[outer_key][key] = {}
+            new_data[outer_key][key] = {k: val.keys() for k, val in value.items()}
+            for k, val in value.items():
+                if len(val.keys()) > 1:
+                    new_data[outer_key][key][k] = [v for v in val.keys()]
+                else:
+                    new_data[outer_key][key][k] = list(val.keys())[0]
+    return new_data
+
 
 
 def prepare_genre(data: dict) -> list[tuple]:
@@ -31,20 +57,32 @@ def prepare_musician(data: dict, labels: dict) -> list[tuple]:
         A list of tuples containing filtered musician data formatted for feeding the `musician`
         table in the `metal_db` database.
     """
+    
     musician_data = [
-    (
-        item_code, # Wikidata_id
-        labels.get(item_code, ""), # Name
-        # Unique Instruments (converted to a list to preserve order) padded with None to ensure 5 instruments     
-        *list(dict.fromkeys(item.get("instrument") for item in item_data.values() if item.get("instrument")))
-        + [None] * (6 - len([item.get("instrument") for item in item_data.values() if item.get("instrument") if item.get("instrument") is not None]))
-    )[:7]
-    # Iterate over each item in the items dictionary and
-    # Filter items based on the condition
+        tuple(
+            [
+                item_code,  # Wikidata_id
+                labels.get(item_code, ""),  # Name
+                *(
+                    # If only one instrument, extract it from the list and pad with None
+                    [instrument] + [None] * (5 - 1) if isinstance(instrument, str) else
+                    # If multiple instruments, use them and pad with None
+                    instrument + [None] * (5 - len(instrument)) if isinstance(instrument, list) else
+                    # If no instrument, fill with None
+                    [None] * 5
+                )
+            ]
+        )
+    # Iterate over each item in the items dictionary
+    # and filter items based on the condition
     for item_code, item_data in data.items()
-    if any(code in (item_data.get(idx, {}).get("item_wikidata", "") for idx in item_data.keys()) 
-           for code in ("Q1792372", "Q36834", "Q5"))
-]
+    if any(
+        code in (item_data.get(idx, {}).get("item_wikidata", "") for idx in item_data.keys())
+        for code in ("Q1792372", "Q36834", "Q5")
+    )
+    # Extract the instrument value from the inner dictionary
+    for instrument in (item.get("instrument") for item in item_data.values())
+    ]
     return musician_data
 
 
@@ -62,17 +100,24 @@ def prepare_band(data: dict, labels: dict) -> list[tuple]:
     """
     band_data = [
     (
-        item_code,
-        # Unique bands (filtered to remove None values and then converted to a list to preserve order)  
-        *list(dict.fromkeys(filter(None, [(labels.get(item_code, ""), item.get("country"), item_code, item.get("start"), \
-                                           item.get("end")) for item in item_data.values()])))
+        labels.get(item_code, ""),
+        item.get("country") if isinstance(item.get("country"), str) else \
+            item.get("country", " ")[0] if isinstance(item.get("country"), list) else \
+            None, 
+        item_code, 
+        item.get("start") if isinstance(item.get("start"), str) else \
+            item.get("start", " ")[0] if isinstance(item.get("start"), list) else \
+            None,
+        item.get("end") if isinstance(item.get("end"), str) else \
+        item.get("end", " ")[-1] if isinstance(item.get("end"), list) else \
+            None
     )
     for item_code, item_data in data.items()
+    for item in item_data.values()
     if any(code in (item_data.get(idx, {}).get("item_wikidata", "") for idx in item_data.keys()) for code in \
            ("Q19351429", "Q7623897", "Q1400264", "Q106581115", "Q105756328", "Q56816954", "Q6942541", "Q9212979", 
             "Q2088357", "Q215380", "Q281643", "Q713200", "Q5741069", "Q7558495", "Q215048", "Q1190668"))
 ]
-    band_data = [band[1] for band in band_data]
     return band_data
 
 
@@ -90,17 +135,26 @@ def prepare_album(data: dict, labels: dict) -> list[tuple]:
     """
     album_data = [
     (
-        item_code,
-        *list(dict.fromkeys(filter(None, [(labels.get(item_code, "")[:100], item.get("performer_wikidata"), \
-                                           item.get("publicationdate"), item.get("duration"), item.get("item"), item_code) \
-                                            for item in item_data.values()])))
+        labels.get(item_code, "")[:100],
+        item.get("performer_wikidata") if isinstance(item.get("performer_wikidata"), str) else \
+            item.get("performer_wikidata", " ")[0] if isinstance(item.get("performer_wikidata"), list) else \
+            None, 
+        item.get("publicationdate") if isinstance(item.get("publicationdate"), str) else \
+            item.get("publicationdate", " ")[0] if isinstance(item.get("publicationdate"), list) else \
+            None,
+        item.get("duration") if isinstance(item.get("duration"), str) else \
+            item.get("duration", " ")[0] if isinstance(item.get("duration"), list) else \
+            None,
+        item.get("item") if isinstance(item.get("item"), str) else \
+            item.get("item", " ")[0] if isinstance(item.get("item"), list) else \
+            None,
+        item_code
     )
     for item_code, item_data in data.items()
-    if any(code in (item_data.get(idx, {}).get("item_wikidata", "") for idx in item_data.keys()) for code in (
-        "Q482994", "Q108352648", "Q20671381", "Q107154516", "Q217199", "Q368281", "Q60713210", "Q208569", "Q10590726"
-        ))
-]
-    album_data = [album[1] for album in album_data]
+    for item in item_data.values()
+    if any(code in (item_data.get(idx, {}).get("item_wikidata", "") for idx in item_data.keys()) for code in \
+           ("Q482994", "Q108352648", "Q20671381", "Q107154516", "Q217199", "Q169930", "Q368281", "Q60713210", "Q208569", "Q10590726"))
+    ]
     return album_data
 
 
@@ -118,17 +172,24 @@ def prepare_song(data: dict, labels: dict) -> list[tuple]:
     """
     song_data = [
     (
-        item_code,
-        *list(dict.fromkeys(filter(None, [(labels.get(item_code, ""), item.get("performer_wikidata"), \
-                                           item.get("album_wikidata"), item.get("duration"), item_code) \
-                                            for item in item_data.values()])))
+        labels.get(item_code, "")[:50],
+        item.get("performer_wikidata") if isinstance(item.get("performer_wikidata"), str) else \
+            item.get("performer_wikidata", " ")[0] if isinstance(item.get("performer_wikidata"), list) else \
+            None, 
+        item.get("album_wikidata") if isinstance(item.get("album_wikidata"), str) else \
+            item.get("album_wikidata", " ")[0] if isinstance(item.get("album_wikidata"), list) else \
+            None,
+        item.get("duration") if isinstance(item.get("duration"), str) else \
+            item.get("duration", " ")[0] if isinstance(item.get("duration"), list) else \
+            None,
+        item_code
     )
     for item_code, item_data in data.items()
-    if any(code in (item_data.get(idx, {}).get("item_wikidata", "") for idx in item_data.keys()) for code in (
-        "Q7302866", "Q155171", "Q220935", "Q59847891", "Q4132319", "Q169930", "Q55850593", "Q55850643", "Q193977", 
-        "Q105543609", "Q56599584", "Q134556", "Q108352496", "Q7366", "Q58232557", "Q677466"))
-]
-    song_data = [song[1] for song in song_data]
+    for item in item_data.values()
+    if any(code in (item_data.get(idx, {}).get("item_wikidata", "") for idx in item_data.keys()) for code in \
+           ("Q7302866", "Q155171", "Q220935", "Q59847891", "Q4132319", "Q55850593", "Q55850643", "Q193977", 
+           "Q105543609", "Q56599584", "Q134556", "Q108352496", "Q7366", "Q58232557", "Q677466"))
+    ]
     return song_data
 
 
@@ -153,13 +214,14 @@ def main() -> dict:
     genre = read_from_json("data/processed/genre_details.json")
     items = read_from_json("data/processed/detailed_items.json")
     labels = read_from_json("data/raw/metal_items.json")
-
+    items = format_items(items)
+    write_to_json(items, "data/processed/formatted_detailed_items.json")
     genres = prepare_genre(genre)
     musicians = prepare_musician(items, labels)
     bands = prepare_band(items, labels)
     albums = prepare_album(items, labels)
     songs = prepare_song(items, labels)
-
+    
     return {'genres': genres, 'musicians': musicians, 'bands': bands, 'albums': albums, 'songs': songs}
 
 
