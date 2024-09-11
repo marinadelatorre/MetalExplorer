f48582aa533645373493f1db74d2453bcd837c3d
Marina Delatorre <marina_mzd@yahoo.com.br>
2024-09-11 09:53:37 +0200

    Improve MetalAPIRequest class

--- src/data/metal_api_request.py
+++ src/data/metal_api_request.py
@@ -3,6 +3,7 @@ import requests
 import requests_cache
 import time
 
+from tqdm import tqdm
 from utils.utils import read_text, write_to_json
 
 
@@ -11,13 +12,16 @@ HEADERS = {
             'accept': 'application/json',
             'user-agent': f'metalexplorer/{PROJECT_VERSION}',
         }
-requests_cache.install_cache()
+requests_cache.install_cache('metal_api_cache')
+
 
 class MetalAPIRequest:
 
-    def __init__(self, identifiers) -> None:
+    def __init__(self, identifiers, save_data=True) -> None:
         self.identifiers = identifiers
+        self.save_data = save_data
         self.ids = self._extract_ids()
+
 
     def _request_from_api(self, endpoint, id):
         response = requests.get(f'https://metal-api.dev/{endpoint}/{id}', headers=HEADERS)
@@ -32,20 +36,26 @@ class MetalAPIRequest:
 
     def _is_cached_result(self, response):
         return getattr(response, 'from_cache', False)
+    
+
+    def _get_data_by_category(self, category):
+        category_responses = []
+
+        for id in tqdm(self.ids[category], total=len(self.ids[category]), \
+                       desc=f"Fetching data for {category}s"):
+            response = self._request_from_api(f'{category}s', id)
+            category_responses.append(response) if response else None
+            write_to_json(response, f'data/raw/{category}/metal_api/{id}.json') \
+                if self.save_data and response else None
+            time.sleep(2) if not self._is_cached_result(response) else None
+
+        return category_responses
 
 
-    def get_data(self, save=True):
-        self.data = {category: {} for category in self.ids.keys()}
-
-        for category, ids in self.ids.items():
-            for id in ids:
-                response = self._request_from_api(f'{category}s', id)
-                if response:
-                    self.data[category].update(response)
-                    write_to_json(self.data[category], f'data/raw/{category}/metal_api/{id}.json') \
-                        if save else None
-                    time.sleep(1) if not self._is_cached_result(response) else None
-
+    def get_data(self):
+        self.data = {
+            category: self._get_data_by_category(category) for category in self.ids.keys()
+            }
         return self.data
     
 
@@ -56,5 +66,4 @@ class MetalAPIRequest:
             [ids[category].add(ids_dict["emid"]) \
                 for _, ids_dict in values.items() \
                 if "emid" in ids_dict.keys() and category in ("band", "album")]
-        print(ids)
         return ids
