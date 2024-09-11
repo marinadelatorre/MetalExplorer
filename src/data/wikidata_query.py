60a651d8f8e1eda5eed53466bce1ad47b2714c29
Marina Delatorre <marina_mzd@yahoo.com.br>
2024-09-11 09:04:12 +0200

    Implement get identifiers

--- src/data/wikidata_query.py
+++ src/data/wikidata_query.py
@@ -12,10 +12,6 @@ ENDPOINT_URL = "https://query.wikidata.org/sparql"
 
 class WikidataQuery:
     def __init__(self):
-        """
-        Args:
-            query (str): The SPARQL query string to execute.
-        """
         queries = read_from_json('config/queries.json')
         self.query_genre = read_text(queries.get('query_genre', None))
         self.query_genre_filter_date = read_text(queries.get('query_genre_filter_date', None))
@@ -23,6 +19,7 @@ class WikidataQuery:
         self.query_items_filter_date = read_text(queries.get('query_items_filter_date', None))
         self.query_details = read_text(queries.get('query_details', None))
         self.query_details_filter_date = read_text(queries.get('query_details_filter_date', None))
+        self.categories = read_from_json("config/category_data.json")
 
 
     def _execute_query(self, query) -> dict:
@@ -88,19 +85,21 @@ class WikidataQuery:
         return item_result
     
 
+    def _select_category(self, item_type: str) -> str:
+        return self.categories.get(item_type, None)
+    
+
     def get_genres(self, save=True) -> list:
         genre_response = self._execute_query(self.query_genre)
         self.genres = self._parse_genre_results(genre_response)
-        write_to_json(self.genres, 'data/raw/genres.json') if save else None
+        write_to_json(self.genres, 'data/raw/wikidata_genres.json') if save else None
         return self.genres
 
 
     def _get_item_labels_by_genre(self, genre):
-        genre_items = {}
         try:
             items = self._fetch_items_from_query(self.query_items, {'genre': genre})
             items = self._parse_item_results(items)
-            genre_items.update(items)
         except urllib.error.HTTPError as e:
             print(f"HTTP Error fetching items for genre '{genre}': {e}")
         except Exception as e:
@@ -108,14 +107,27 @@ class WikidataQuery:
         return items
     
 
+    def _clean_item_labels(self):
+        for item in list(self.all_items.keys()):
+            if item not in self.categories.keys():
+                self.all_items.pop(item)
+            elif item not in self.categories.values():
+                self.all_items[self.categories[item]].update(self.all_items.get(item)) \
+                    if self.categories[item] in self.all_items.keys() \
+                    else self.all_items.update({self.categories[item]: self.all_items.get(item)})
+                self.all_items.pop(item)
+
     def get_all_item_labels(self, save=True):
         self.all_items = {}
         {self.all_items.update(self._get_item_labels_by_genre(genre)) \
-                          for genre in tqdm(list(self.genres.keys()), total=len(self.genres))}
-        # self.all_items = {self._get_item_labels_by_genre(genre) \
-        #                   for genre in tqdm(list(self.genres.keys()), total=len(self.genres))}
+                          for genre in tqdm(list(self.genres.keys()), \
+                                            total=len(self.genres), \
+                                            desc="Fetching items by genre")}
+        print("Total number of items", len(self.all_items))
+        write_to_json(self.all_items, f"data/raw/wikidata_all_item_labels.json")
+        self._clean_item_labels()
         
-        [write_to_json(item, f"data/raw/{type_label}/item_labels.json") \
+        [write_to_json(item, f"data/raw/{type_label}/wikidata_item_labels.json") \
             for type_label, item in self.all_items.items()] \
             if save else None
         return self.all_items
@@ -123,7 +135,7 @@ class WikidataQuery:
 
     def _get_details_by_type(self, items):
         item_details = {}
-        for item, label in tqdm(items.items(), total=len(items)): 
+        for item, label in tqdm(items.items(), total=len(items), desc="Fetching item details"): 
             try:
                 details = self._fetch_items_from_query(self.query_details, {'item': item})
                 item_details.update({label: details})
@@ -139,7 +151,33 @@ class WikidataQuery:
             type_label: self._get_details_by_type(items)
             for type_label, items in self.all_items.items()
         }
-        [write_to_json(item, f"data/raw/{type_label}/items_details.json") \
+        [write_to_json(item, f"data/raw/{type_label}/wikidata_items_details.json") \
             for type_label, item in self.all_items_details.items()] \
             if save else None
         return self.all_items_details
+    
+
+    def _get_identifiers_by_category(self, value):
+        index = {}
+        for id, details in value.items():
+            unpacked_details = details['results']['bindings'][0]
+            if unpacked_details.get("mbid", None):
+                index.update({id: {}}) if id not in index else None
+                index[id].update({'mbid': unpacked_details["mbid"]["value"]})
+            if unpacked_details.get("emid", None):
+                index.update({id: {}}) if id not in index else None
+                index[id].update({'emid': unpacked_details["emid"]["value"]})
+        return index
+    
+
+    def get_identifiers(self, save=True):
+        self.identifiers = {category: {} for category in set(self.categories.values())}
+
+        for category in set(self.categories.values()):
+            [self.identifiers[category].update(self._get_identifiers_by_category(value)) \
+            for _, value in self.all_items_details[category].items()]
+
+            write_to_json(self.identifiers[category], f"data/raw/{category}/wikidata_identifiers.json") \
+            if save and len(self.identifiers[category]) > 0 else self.identifiers.pop(category)
+        
+        return self.identifiers
